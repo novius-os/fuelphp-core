746dbbf809de10090e91b8d9fed93b954530e2bb
WanWizard <wanwizard@wanwizard.eu>
2011-06-06 22:55:33 +0200

    implemented TRUNCATE for MySQL/MySQLi when DELETE FROM is used without WHERE or LIMIT

--- classes/database/mysql/builder/delete.php
+++ classes/database/mysql/builder/delete.php
@@ -14,5 +14,27 @@ namespace Fuel\Core;
 
 class Database_MySQLi_Builder_Delete extends \Database_Query_Builder_Delete {
 
+	/**
+	 * Compile the SQL query and return it.
+	 *
+	 * @param   object  Database instance
+	 * @return  string
+	 */
+	public function compile(\Database_Connection $db)
+	{
+		// see if we can get away with a truncate
+		if ( empty($this->_where) and is_null($this->_limit))
+		{
+			// use truncate instead
+			$query = 'TRUNCATE '.$db->quote_table($this->_table);
+		}
+		else
+		{
+			$query = parent::compile($db);
+		}
+
+		return $query;
+	}
+
 }
 // End Database_MySQLi_Builder_Delete
