7d48caa224517ec7e1bbdceeb80e4ad9cb7c978e
Sébastien CAPARROS <caparros@novius.fr>
2016-01-20 14:10:38 +0100

    Added french translation

---
+++ lang/fr/date.php
@@ -0,0 +1,29 @@
+<?php
+
+return array(
+	'text' => 'Il y a :time',
+	
+	'second' => ':t seconde',
+	'seconds' => ':t secondes',
+	
+	'minute' => ':t minute',
+	'minutes' => ':t minutes',
+	
+	'hour' => ':t heure',
+	'hours' => ':t heures',
+	
+	'day' => ':t jour',
+	'days' => ':t jours',
+	
+	'week' => ':t semaine',
+	'weeks' => ':t semaines',
+	
+	'month' => ':t mois',
+	'months' => ':t mois',
+	
+	'year' => ':t an',
+	'years' => ':t ans',
+	
+	'decade' => ':t décennie',
+	'decades' => ':t décennies',
+);
