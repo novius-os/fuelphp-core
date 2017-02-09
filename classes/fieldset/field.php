468b9f2cfb1b2b0fea1ccde42c976944b7ef8bc6
romu <chollet@novius.fr>
2017-02-09 11:47:19 +0100

    fix add radio/checkbox input  multiple (with options config fields)

--- classes/fieldset/field.php
+++ classes/fieldset/field.php
@@ -571,55 +571,67 @@ class Fieldset_Field
 		return $this->template($build_field);
 	}
 
-	protected function template($build_field)
-	{
-		$form = $this->fieldset()->form();
+    protected function template($build_field)
+    {
+        $form = $this->fieldset()->form();
 
-		$required_mark = $this->get_attribute('required', null) ? $form->get_config('required_mark', null) : null;
-		$label = $this->label ? $form->label($this->label, null, array('id' => 'label_'.$this->name, 'for' => $this->get_attribute('id', null), 'class' => $form->get_config('label_class', null))) : '';
-		$error_template = $form->get_config('error_template', '');
-		$error_msg = ($form->get_config('inline_errors') && $this->error()) ? str_replace('{error_msg}', $this->error(), $error_template) : '';
-		$error_class = $this->error() ? $form->get_config('error_class') : '';
+        $required_mark = $this->get_attribute('required', null) ? $form->get_config('required_mark', null) : null;
+        $label = $this->label ? $form->label($this->label, null, array('id' => 'label_'.$this->name, 'for' => $this->get_attribute('id', null), 'class' => $form->get_config('label_class', null))) : '';
+        $error_template = $form->get_config('error_template', '');
+        $error_msg = ($form->get_config('inline_errors') && $this->error()) ? str_replace('{error_msg}', $this->error(), $error_template) : '';
+        $error_class = $this->error() ? $form->get_config('error_class') : '';
 
-		if (is_array($build_field))
-		{
-			$label = $this->label ? str_replace('{label}', $this->label, $form->get_config('group_label', '<span>{label}</span>')) : '';
-			$template = $this->template ?: $form->get_config('multi_field_template', "\t\t<tr>\n\t\t\t<td class=\"{error_class}\">{group_label}{required}</td>\n\t\t\t<td class=\"{error_class}\">{fields}\n\t\t\t\t{field} {label}<br />\n{fields}\t\t\t{error_msg}\n\t\t\t</td>\n\t\t</tr>\n");
-			if ($template && preg_match('#\{fields\}(.*)\{fields\}#Dus', $template, $match) > 0)
-			{
-				$build_fields = '';
-				foreach ($build_field as $lbl => $bf)
-				{
-					$bf_temp = str_replace('{label}', $lbl, $match[1]);
-					$bf_temp = str_replace('{required}', $required_mark, $bf_temp);
-					$bf_temp = str_replace('{field}', $bf, $bf_temp);
-					$build_fields .= $bf_temp;
-				}
+        if (is_array($build_field))
+        {
+            $tagLabel = '{label}';
+            if(is_array($this->label)) {
+                $tagLabel = array('{label}');
+            }
 
-				$template = str_replace($match[0], '{fields}', $template);
-				$template = str_replace(array('{group_label}', '{required}', '{fields}', '{error_msg}', '{error_class}', '{description}'), array($label, $required_mark, $build_fields, $error_msg, $error_class, $this->description), $template);
+            $labelName = $this->label ? str_replace($tagLabel, $this->label, $form->get_config('group_label', '<span>{label}</span>')) : '';
+            $label = $form->label($labelName, null, array('id' => 'label_'.$this->name));
 
-				return $template;
-			}
+            $template = $this->template ?: $form->get_config('multi_field_template', "\t\t<tr>\n\t\t\t<td class=\"{error_class}\">{group_label}{required}</td>\n\t\t\t<td class=\"{error_class}\">{fields}\n\t\t\t\t{field} {label}<br />\n{fields}\t\t\t{error_msg}\n\t\t\t</td>\n\t\t</tr>\n");
+            if ($template && preg_match('#\{fields\}(.*)\{fields\}#Dus', $template, $match) > 0)
+            {
+                $build_fields = '';
+                foreach ($build_field as $lbl => $bf)
+                {
+                    $bf_temp = str_replace('{label}', $lbl, $match[1]);
+                    $bf_temp = str_replace('{required}', $required_mark, $bf_temp);
+                    $bf_temp = str_replace('{field}', $bf, $bf_temp);
+                    $build_fields .= $bf_temp;
+                }
 
-			// still here? wasn't a multi field template available, try the normal one with imploded $build_field
-			$build_field = implode(' ', $build_field);
-		}
+                $template = str_replace($match[0], '{fields}', $template);
+                $template = str_replace(array('{group_label}', '{required}', '{fields}', '{error_msg}', '{error_class}', '{description}'), array($label, $required_mark, $build_fields, $error_msg, $error_class, $this->description), $template);
 
-		// determine the field_id, which allows us to identify the field for CSS purposes
-		$field_id = 'col_'.$this->name;
-		if ($parent = $this->fieldset()->parent())
-		{
-			$parent->get_tabular_form() and $field_id = $parent->get_tabular_form().'_col_'.$this->basename;
-		}
+                return $template;
+            }
 
-		$template = $this->template ?: $form->get_config('field_template', "\t\t<tr>\n\t\t\t<td class=\"{error_class}\">{label}{required}</td>\n\t\t\t<td class=\"{error_class}\">{field} {description} {error_msg}</td>\n\t\t</tr>\n");
-		$template = str_replace(array('{label}', '{required}', '{field}', '{error_msg}', '{error_class}', '{description}', '{field_id}'),
-			array($label, $required_mark, $build_field, $error_msg, $error_class, $this->description, $field_id),
-			$template);
+            // map the key value $build_field (to have <input...>label)
+            $labels = array_keys($build_field);
+            $buildFieldMap = array_map(function($value, $key) {
+                return $value.$key;
+            }, $build_field, $labels);
 
-		return $template;
-	}
+            $build_field = implode(' ', $buildFieldMap);
+        }
+
+        // determine the field_id, which allows us to identify the field for CSS purposes
+        $field_id = 'col_'.$this->name;
+        if ($parent = $this->fieldset()->parent())
+        {
+            $parent->get_tabular_form() and $field_id = $parent->get_tabular_form().'_col_'.$this->basename;
+        }
+
+        $template = $this->template ?: $form->get_config('field_template', "\t\t<tr>\n\t\t\t<td class=\"{error_class}\">{label}{required}</td>\n\t\t\t<td class=\"{error_class}\">{field} {description} {error_msg}</td>\n\t\t</tr>\n");
+        $template = str_replace(array('{label}', '{required}', '{field}', '{error_msg}', '{error_class}', '{description}', '{field_id}'),
+            array($label, $required_mark, $build_field, $error_msg, $error_class, $this->description, $field_id),
+            $template);
+
+        return $template;
+    }
 
 	/**
 	 * Alias for $this->fieldset->validation->input() for this field
